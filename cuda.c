645edf5eb5ef695a48a7ea2301e1f611dcba355f
Patrick Harris <pdharris@udel.edu>
2023-05-02 16:35:47 -0400

    Adding in beginning of Cuda Files

---
+++ cuda.c
@@ -0,0 +1,11 @@
+/*
+	CISC372 Assignment 4: Lost in space 
+	Contributors: Patrick Harris, Robert Reardon
+	File: cuda.c
+*/
+
+#include <stdlib.h>
+#include <math.h>
+#include "vector.h"
+#include "config.h"
+#include "cuda.h"
---
+++ cuda.h
@@ -0,0 +1,22 @@
+/*
+	CISC372 Assignment 4: Lost in space 
+	Contributors: Patrick Harris, Robert Reardon
+	File: cuda.h
+*/
+
+#ifndef CUDA_H
+#define CUDA_H
+
+// Include necessary libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+
+// Define constants
+#define N 10
+#define THREADS_PER_BLOCK 2
+
+// Declare functions
+
+
+#endif /* CUDA_H */
