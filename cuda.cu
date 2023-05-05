ae379be65f3abbe990d37642a1ccc11b042a0fd0
Patrick Harris <pdharris@udel.edu>
2023-05-05 12:25:24 -0400

    Changing file to cuda file

--- cuda.cu
+++ cuda.cu
@@ -1,7 +1,7 @@
 /*
 	CISC372 Assignment 4: Lost in space 
 	Contributors: Patrick Harris, Robert Reardon
-	File: cuda.c
+	File: cuda.cu
 */
 
 #include <stdlib.h>
@@ -9,3 +9,4 @@
 #include "vector.h"
 #include "config.h"
 #include "cuda.h"
+#include <cuda_runtime.h>
