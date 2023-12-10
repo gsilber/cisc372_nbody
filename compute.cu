a9b7efdc5330cf34bfee8feabd404b1fad157c66
michael <ydna.mjs@gmail.com>
2023-12-10 09:57:28 -0500

    deleted a bunch of unused commented out code

--- compute.cu
+++ compute.cu
@@ -23,17 +23,11 @@ __global__ void test_kernel(vector3 *accels, vector3* pos, double* mass) {
     __syncthreads();
 
     if(i < NUMENTITIES && j < NUMENTITIES) {
-        //accels[i * NUMENTITIES + j][threadIdx.z] = i;
 
         if (i == j) {
             accels[i * NUMENTITIES + j][threadIdx.z] = 0.0;
         }
         else{
-  /*           double dx = pos[i][0] - pos[j][0];
-            double dy = pos[i][1] - pos[j][1];
-            double dz = pos[i][2] - pos[j][2]; */
-
-            //double magnitude_sq = dx * dx + dy * dy + dz * dz;
             double magnitude_sq = ( 
                 distances[threadIdx.x][threadIdx.y][0] * distances[threadIdx.x][threadIdx.y][0] + 
                 distances[threadIdx.x][threadIdx.y][1] * distances[threadIdx.x][threadIdx.y][1] + 
@@ -42,15 +36,9 @@ __global__ void test_kernel(vector3 *accels, vector3* pos, double* mass) {
 
             double magnitude = sqrt(magnitude_sq);
             double accelmag =- 1 * GRAV_CONSTANT * mass[j] / magnitude_sq;
-            //accels[i * NUMENTITIES + j][threadIdx.z] = accelmag * dx / magnitude; //I SHOULD NOT BE DX FOR ALL threadIdx.z
             accels[i * NUMENTITIES + j][threadIdx.z] = accelmag * distances[threadIdx.x][threadIdx.y][threadIdx.z] / magnitude;
-            //accels[i * NUMENTITIES + j][threadIdx.z] = distances[threadIdx.x][threadIdx.y][threadIdx.z];
         }
     }
-
-/*     vel[threadIdx.x][0] = vel[threadIdx.x][0] + 1.0;
-    pos[threadIdx.x][0] = pos[threadIdx.x][0] + 2.0;
-    mass[threadIdx.x] = mass[threadIdx.x] + 3.0; */
 }
 
 //compute: Updates the positions and locations of the objects in the system based on gravity.
@@ -94,28 +82,7 @@ void compute(){
     printf("hVel: %.1f | hPos: %.1f | hmass: %.1f\n\n", hVel[1][0], hPos[1][0], hmass[1]); */
 
 
-/* 	//make an acceleration matrix which is NUMENTITIES squared in size;
-	int i,j,k;
-	vector3* values=(vector3*)malloc(sizeof(vector3)*NUMENTITIES*NUMENTITIES);
-	vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
-	for (i=0;i<NUMENTITIES;i++)
-		accels[i]=&values[i*NUMENTITIES];
-	//first compute the pairwise accelerations.  Effect is on the first argument.
-	for (i=0;i<NUMENTITIES;i++){
-		for (j=0;j<NUMENTITIES;j++){
-			if (i==j) {
-				FILL_VECTOR(accels[i][j],0,0,0);
-			}
-			else{
-				vector3 distance;
-				for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
-				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
-				double magnitude=sqrt(magnitude_sq);
-				double accelmag=-1*GRAV_CONSTANT*hmass[j]/magnitude_sq;
-				FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
-			}
-		}
-	}
+/*
 	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
 	for (i=0;i<NUMENTITIES;i++){
 		vector3 accel_sum={0,0,0};
